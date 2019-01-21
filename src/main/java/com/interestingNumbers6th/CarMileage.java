54ca2c4b762af96e077435f44a9b8fdc4925d377
Levinzhong <Levin.zhong@oocl.com>
2019-01-21 16:45:05 +0800

    input a number 100 and empty array and should return 2,fix test fail

--- src/main/java/com/interestingNumbers6th/CarMileage.java
+++ src/main/java/com/interestingNumbers6th/CarMileage.java
@@ -2,7 +2,23 @@ package com.interestingNumbers6th;
 
 class CarMileage {
 
+  static int interesting = 2;
+  static int boring = 0;
+
   int isInteresting(int mileage, int[] awesomePhrases) {
-    return 0;
+    if (isFollowedByAllZeros(mileage)) {
+      return interesting;
+    }
+    return boring;
+  }
+
+  private boolean isFollowedByAllZeros(int num) {
+    while ((num % 10) == 0) {
+      num = num / 10;
+      if (num < 10) {
+        return true;
+      }
+    }
+    return false;
   }
 }
