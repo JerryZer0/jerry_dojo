68bef7497da76e021323f2193905f09088c058b2
jerry <jerry@oocl.com>
2019-02-19 19:59:55 +0800

    pass case Two-Love add score map

--- src/main/java/com/tennisScoring8th/TennisScoreboard.java
+++ src/main/java/com/tennisScoring8th/TennisScoreboard.java
@@ -1,9 +1,21 @@
 package com.tennisScoring8th;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class TennisScoreboard {
 
   private int playerScore1;
   private int playerScore2;
+
+  private static final Map<Integer, String> SCORE = new HashMap<>();
+
+  static {
+    SCORE.put(0, "Love");
+    SCORE.put(1, "One");
+    SCORE.put(2, "Two");
+    SCORE.put(3, "Three");
+  }
 
   TennisScoreboard(int playerScore1, int playerScore2) {
     this.playerScore1 = playerScore1;
@@ -11,6 +23,6 @@ class TennisScoreboard {
   }
 
   public String calculate() {
-    return "One-Love";
+    return SCORE.get(playerScore1)+"-"+SCORE.get(playerScore2);
   }
 }
